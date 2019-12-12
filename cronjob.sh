e02f6d2a603f96ee6a75f60d2e9dd0ccedd6f388
alturismo <mabenamara@freenet.de>
2019-12-12 08:01:04 +0100

    Update cronjob.sh

--- cronjob.sh
+++ cronjob.sh
@@ -4,6 +4,8 @@
 
 use_guide2go="yes"
 use_xTeveAPI="yes"
+use_embyAPI="no"
+use_plexAPI="no"
 use_TVH_Play="no"
 use_TVH_move="no"
 
@@ -17,6 +19,24 @@ JsonList="CBLguide.json SATguide.json SATSport.json"
 ### xTeve ip, Port in case API is used to update XEPG
 xTeveIP="192.168.1.2"
 xTevePORT="34400"
+
+### Emby ip, Port, apiKey, update ID in case API is used to update EPG directly after guide2go
+# ONLY when xteve API is in use, otherwise obsolete
+# API Key, https://github.com/MediaBrowser/Emby/wiki/Api-Key-Authentication
+# embyID, settings, scroll down click API, Scheduled Task Service, GET /ScheduledTasks, Try, Execute, look for "Refresh Guide" ID, sample here 9492d30c70f7f1bec3757c9d0a4feb45
+embyIP="192.168.1.2"
+embyPORT="8096"
+embyApiKey=""
+embyID="9492d30c70f7f1bec3757c9d0a4feb45"
+
+### Plex ip, Port, Token, TV Section ID in case API is used to update EPG directly after guide2go
+# ONLY when xteve API is in use, otherwise obsolete
+# Plex Token, https://support.plex.tv/articles/204059436-finding-an-authentication-token-x-plex-token/
+# plexID, http://YOUR_IP_HERE:32400/?X-Plex-Token=YOUR_TOKEN_HERE , look for "tv.plex.providers.epg.xmltv:  ", sample here 11
+plexIP="192.168.1.2"
+plexPORT="32400"
+plexToken=""
+plexID="11"
 
 ### TVHeadend ip, Port in case m3u Playlist is wanted
 TVHIP="192.168.1.2"
@@ -83,6 +103,26 @@ if [ "$use_TVH_move" = "yes" ]; then
 		echo "no Path credential"
 	else
 		cp $TVHSOURCE $TVHPATH/guide.xml
+	fi
+fi
+
+# update Emby via API
+if [ "$use_embyAPI" = "yes" ]; then
+	if [ -z "$embyIP" ]; then
+		echo "no Emby credentials"
+	else
+		curl -X POST "http://$embyIP:$embyPORT/emby/ScheduledTasks/Running/$embyID?api_key=$embyApiKey" -H "accept: */*" -d ""
+		sleep 1
+	fi
+fi
+
+# update Plex via API
+if [ "$use_plexAPI" = "yes" ]; then
+	if [ -z "$plexIP" ]; then
+		echo "no Plex credentials"
+	else
+		curl -s "http://$plexIP:$plexPORT/livetv/dvrs/$plexID/reloadGuide?X-Plex-Product=Plex%20Web&X-Plex-Version=4.8.4&X-Plex-Client-Identifier=$plexToken&X-Plex-Platform=Firefox&X-Plex-Platform-Version=69.0&X-Plex-Sync-Version=2&X-Plex-Features=external-media&X-Plex-Model=bundled&X-Plex-Device=Linux&X-Plex-Device-Name=Firefox&X-Plex-Device-Screen-Resolution=1128x657%2C1128x752&X-Plex-Language=de" -H "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0" -H "Accept: text/plain, */*; q=0.01" -H "Accept-Language: de" --compressed -H "X-Requested-With: XMLHttpRequest" -H "Connection: keep-alive" -H "Referer: http://$plexIP:$plexPORT/web/index.html" --data ""
+		sleep 1
 	fi
 fi
 
